ee8b8eca465ec160207a10e0a8cb930bf67214f9
yoshihiko <yoshihiko05410@gmail.com>
2026-01-27 01:04:50 +0900

    chore(cask): update sha256 for v0.0.1

--- Casks/nudge.rb
+++ Casks/nudge.rb
@@ -1,6 +1,6 @@
 cask "nudge" do
   version "0.0.1"
-  sha256 "REPLACE_WITH_SHA256"
+  sha256 "ca9cc3d9b85999f95498805db599ea571c616fd1d04337cb9816b37b48ab6e37"
 
   url "https://github.com/yoshihiko555/Nudge/releases/download/v#{version}/Nudge-#{version}.dmg"
   name "Nudge"
